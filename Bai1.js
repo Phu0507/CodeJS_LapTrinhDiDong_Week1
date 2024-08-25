24df4954e2d1f7a4ad658aa2dd571273a2c39d6f
H82M19\Student <Student>
2024-08-25 10:48:13 +0700

    week1 js ex1+2

--- Bai1.js
+++ Bai1.js
@@ -44,3 +44,4 @@ if(markHigherBMI === true)
     console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
 else
     console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+
